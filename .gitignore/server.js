9b8dcc63d9ce3d640a8e00e54e2545faeb2994b5
lehoangvu <hoangvu171819@gmail.com>
2018-03-14 05:16:50 +0700

    Update server.js

--- .gitignore/server.js
+++ .gitignore/server.js
@@ -1,1 +1,19 @@
 
+var http = require("http");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Grip-Hold': 'stream',
+        'Grip-Channel': 'mychannel'
+      ,
+        'Grip-Keep-Alive': ':\\n\\n; format=cstring; timeout=20'
+    });
+
+    // 2k padding for old browsers
+    var padding = new Array(2048);
+    res.write(':' + padding.join(' ') + '\n');
+
+    res.end();
+}).listen(8081	, '0.0.0.0');
